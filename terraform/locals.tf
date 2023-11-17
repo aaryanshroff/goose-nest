c07f6b7f558277e2f5d4363eda378fc01c13f170
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 01:41:08 -0500

    Create payloads in project root

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  goose_nest_scraper_lambda_layer_payload_path = "${path.module}/goose_nest_scraper_lambda_layer_payload.zip"
-  goose_nest_scraper_lambda_function_payload_path = "${path.module}/goose_nest_scraper_lambda_function_payload.zip"
+  goose_nest_scraper_lambda_layer_payload_path = "${path.module}/../goose_nest_scraper_lambda_layer_payload.zip"
+  goose_nest_scraper_lambda_function_payload_path = "${path.module}/../goose_nest_scraper_lambda_function_payload.zip"
 }
