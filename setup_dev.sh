66da7c2708619251c2d0ce23bc0456e545d90d2d
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 00:19:22 -0500

    Coloured bash messages

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NC=$(tput sgr0)
+
 VENV_PATH=".venv"
 
 # Check if the virtual environment directory exists
 if [ -d "$VENV_PATH" ]; then
-    echo "Virtual environment already exists at $VENV_PATH"
+    echo "Virtual environment already exists at ${VENV_PATH}"
 else
     # If it doesn't exist, create it
     echo "Creating virtual environment at $VENV_PATH"
     python3 -m venv "$VENV_PATH"
+    echo "${GREEN}Virtual environment created.${NC}"
 fi
 
 # Activate the virtual environment
@@ -19,7 +24,7 @@ pip3 install -r src/requirements.txt >/dev/null 2>&1
 
 # Check the exit status of pip install
 if [ $? -eq 0 ]; then
-    echo "Dependencies installed successfully."
+    echo "${GREEN}Dependencies installed successfully.${NC}"
 else
-    echo "Error: Failed to install dependencies."
+    echo -e "${RED}Error: Failed to install dependencies.${NC}"
 fi
