bb084100c95fe243fe5f2a5934224763c33da416
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 02:12:08 -0500

    Use local wherever possible

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,6 +4,7 @@
 resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
   filename   = local.goose_nest_scraper_lambda_layer_payload_path
   layer_name = "goose_nest_scraper_lambda_layer"
+  source_code_hash = filesha256(local.goose_nest_scraper_lambda_layer_payload_path)
 
   compatible_runtimes = ["python3.11"]
 
@@ -14,14 +15,14 @@ resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
 # Goose Nest Scraper Lambda Function
 ##############################################
 data "archive_file" "goose_nest_scraper_lambda_function_payload" {
-  output_path = "goose_nest_scraper_lambda_function_payload.zip"
+  output_path = local.goose_nest_scraper_lambda_function_payload_path
   type = "zip"
 
   source_dir = "${path.module}/../src"
 }
 
 resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
-  filename      = "goose_nest_scraper_lambda_function_payload.zip"
+  filename      = local.goose_nest_scraper_lambda_function_payload_path
   function_name = "goose_nest_scraper_lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
