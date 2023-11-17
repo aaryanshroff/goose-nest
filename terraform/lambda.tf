36eb2982803dcbacedd44d0f0c68a62f4d299a65
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 01:28:11 -0500

    Fix: Lambda layer not updating, lambda timing out

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,6 +6,8 @@ resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
   layer_name = "goose_nest_scraper_lambda_layer"
 
   compatible_runtimes = ["python3.11"]
+
+  skip_destroy = true
 }
 
 ##############################################
@@ -23,6 +25,7 @@ resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
   function_name = "goose_nest_scraper_lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
+  timeout = 60
 
   source_code_hash = data.archive_file.goose_nest_scraper_lambda_function_payload.output_base64sha256
 
