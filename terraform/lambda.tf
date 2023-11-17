f3cb14b03571e9cbd4e42f13191cca67246b199a
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 01:14:40 -0500

    Remove payload creation from TF file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,39 +1,11 @@
 ##############################################
 # Goose Nest Scraper Lambda Layer
 ##############################################
-
-# So that we can conditionally create a new lambda layer payload if requirements.txt changes
-data "local_file" "requirements_dot_txt" {
-  filename = "../src/requirements.txt"
-}
-
-resource "null_resource" "create_goose_nest_scraper_lambda_layer_payload" {
-  triggers = {
-    # Trigger the script execution only if the hash of requirements.txt changes
-    file_changed = md5(data.local_file.requirements_dot_txt.content)
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/../scripts/create_lambda_layer_payload.sh -r src/requirements.txt -o ${local.goose_nest_scraper_lambda_layer_payload_path}"
-  }
-}
-
 resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
   filename   = local.goose_nest_scraper_lambda_layer_payload_path
   layer_name = "goose_nest_scraper_lambda_layer"
 
   compatible_runtimes = ["python3.11"]
-
-  depends_on = [null_resource.create_goose_nest_scraper_lambda_layer_payload]
-}
-
-# Clean up payload zip file
-resource "null_resource" "destroy_goose_nest_scraper_lambda_layer_payload" {
-  depends_on = [aws_lambda_layer_version.goose_nest_scraper_lambda_layer]
-
-  provisioner "local-exec" {
-    command = "rm -rf ${local.goose_nest_scraper_lambda_layer_payload_path}"
-  }
 }
 
 ##############################################
@@ -59,13 +31,4 @@ resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
   layers = [ aws_lambda_layer_version.goose_nest_scraper_lambda_layer.arn ]
 
   depends_on = [ data.archive_file.goose_nest_scraper_lambda_function_payload ]
-}
-
-# Clean up payload zip file
-resource "null_resource" "destroy_goose_nest_scraper_lambda_function_payload" {
-  depends_on = [aws_lambda_function.goose_nest_scraper_lambda_function]
-
-  provisioner "local-exec" {
-    command = "rm -rf ${local.goose_nest_scraper_lambda_function_payload_path}"
-  }
-}
+}d
