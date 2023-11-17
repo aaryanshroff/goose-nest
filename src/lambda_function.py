b5f905ee589802ffdac848835879d6dad7d031a6
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 02:11:51 -0500

    Fix: geckodriver not found

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 def lambda_handler(event, context):
-  driver = webdriver.Firefox()
+  service = webdriver.FirefoxService(executable_path="/opt/python/geckodriver")
+  driver = webdriver.Firefox(service=service)
 
-  return "Hello, Lambda"
+  return "Hello, Selenium"
