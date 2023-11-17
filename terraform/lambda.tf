a671ff98f4a40c7cca88f111ef1282f48214f975
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-16 23:54:43 -0500

    Add locals.tf and comments

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,24 +1,37 @@
+##############################################
+# Goose Nest Scraper Lambda layer
+##############################################
+
 # So that we can conditionally create a new lambda layer payload if requirements.txt changes
 data "local_file" "requirements_dot_txt" {
   filename = "../src/requirements.txt"
 }
 
-resource "null_resource" "goose_nest_scraper_lambda_layer_payload" {
+resource "null_resource" "create_goose_nest_scraper_lambda_layer_payload" {
   triggers = {
     # Trigger the script execution only if the hash of requirements.txt changes
     file_changed = md5(data.local_file.requirements_dot_txt.content)
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/../scripts/create_lambda_layer_payload.sh -r src/requirements.txt -o ${path.module}/goose_nest_scraper_lambda_layer_payload.zip"
+    command = "${path.module}/../scripts/create_lambda_layer_payload.sh -r src/requirements.txt -o ${local.goose_nest_scraper_lambda_layer_payload_path}"
   }
 }
 
 resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
-  filename   = "${path.module}/goose_nest_scraper_lambda_layer_payload.zip"
+  filename   = local.goose_nest_scraper_lambda_layer_payload_path
   layer_name = "goose_nest_scraper_lambda_layer"
 
   compatible_runtimes = ["python3.11"]
 
   depends_on = [null_resource.goose_nest_scraper_lambda_layer_payload]
+}
+
+# Clean up payload zip file
+resource "null_resource" "cleanup_goose_nest_scraper_lambda_layer_payload" {
+  depends_on = [aws_lambda_layer_version.goose_nest_scraper_lambda_layer]
+
+  provisioner "local-exec" {
+    command = "rm -rf ${local.goose_nest_scraper_lambda_layer_payload_path}"
+  }
 }
---
+++ terraform/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  goose_nest_scraper_lambda_layer_payload_path = "${path.module}/goose_nest_scraper_lambda_layer_payload.zip"
+}
