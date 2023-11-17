28d15e5b9fb9e1962f896c644c75e9fbb9e82678
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-17 00:29:37 -0500

    Goose Nest Scraper Lambda Function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -24,14 +24,37 @@ resource "aws_lambda_layer_version" "goose_nest_scraper_lambda_layer" {
 
   compatible_runtimes = ["python3.11"]
 
-  depends_on = [null_resource.goose_nest_scraper_lambda_layer_payload]
+  depends_on = [null_resource.create_goose_nest_scraper_lambda_layer_payload]
 }
 
 # Clean up payload zip file
-resource "null_resource" "cleanup_goose_nest_scraper_lambda_layer_payload" {
+resource "null_resource" "destroy_goose_nest_scraper_lambda_layer_payload" {
   depends_on = [aws_lambda_layer_version.goose_nest_scraper_lambda_layer]
 
   provisioner "local-exec" {
     command = "rm -rf ${local.goose_nest_scraper_lambda_layer_payload_path}"
   }
+}
+
+##############################################
+# Goose Nest Scraper Lambda Function
+##############################################
+data "archive_file" "goose_nest_scraper_lambda_function_payload" {
+  output_path = "goose_nest_scraper_lambda_function_payload.zip"
+  type = "zip"
+
+  source_dir = "${path.module}/../src"
+}
+
+resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
+  filename      = "goose_nest_scraper_lambda_function_payload.zip"
+  function_name = "goose_nest_scraper_lambda_function"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.goose_nest_scraper_lambda_function_payload.output_base64sha256
+
+  runtime = "python3.11"
+
+  depends_on = [ data.archive_file.goose_nest_scraper_lambda_function_payload ]
 }
