7b99907173f3f8ae4cd1088940a293d8700bdeb2
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-23 01:04:35 -0500

    Specify paths to browser and driver

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 
 def lambda_handler(event, context):
   options = webdriver.ChromeOptions()
+  options.binary_location = "/bin/headless-chromium"
   options.add_argument("--headless")
 
-  browser = webdriver.Chrome(options=options)
+  service = webdriver.ChromeService(executable_path="/bin/chromedriver")
+
+  browser = webdriver.Chrome(service=service, options=options)
 
   return "Hello, Selenium"
