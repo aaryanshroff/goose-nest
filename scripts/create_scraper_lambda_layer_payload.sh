e27964a6638e97a1d50b3abbad0db34ad09edaf4
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-23 00:54:29 -0500

    Separate layers for Chrome and Python deps

--- scripts/create_scraper_lambda_layer_payload.sh
+++ scripts/create_scraper_lambda_layer_payload.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+#===============================================================================
+# This script builds two Lambda layers (due to layer size being capped at 250MB)
+# 1. Python dependencies (requests, selenium, etc.)
+# 2. Chrome and chromedriver
+#===============================================================================
 
 set -e # Exit if any command fails (returns non-zero exit code)
 
 # Check if the required arguments are provided
-while getopts ":r:o:" opt; do
+while getopts ":r:p:c:" opt; do
     case $opt in
         r)
             requirements_file="$OPTARG"
             ;;
-        o)
-            output_path="$OPTARG"
+        p)
+            python_layer_output="$OPTARG"
+            ;;
+        c)
+            chrome_layer_output="$OPTARG"
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -22,33 +30,60 @@ while getopts ":r:o:" opt; do
     esac
 done
 
-# Check if both -r and -o are provided
-if [ -z "$requirements_file" ] || [ -z "$output_path" ]; then
-    echo "Usage: $0 -r <path/to/requirements.txt> -o <output/path/layer.zip>"
+# Check if all required arguments are provided
+if [ -z "$requirements_file" ] || [ -z "$python_layer_output" ] || [ -z "$chrome_layer_output" ]; then
+    echo "Usage: $0 -r <path/to/requirements.txt> -p <output/dir/python_layer> -c <output/dir/chrome_layer>"
     exit 1
 fi
 
 project_root=$(pwd | sed 's|\(.*\/goose-nest\).*|\1|')
 
-# Build the Lambda layer using the Lambda Docker image
+cd $project_root
+
+#===============================================================================
+# PYTHON LAYER
+#===============================================================================
+
+# Build the Python Lambda layer using the Lambda Docker image
 docker run \
   -v "$project_root":/var/task \
   "public.ecr.aws/sam/build-python3.11" \
   /bin/sh -c "pip install -r $requirements_file -t python/lib/python3.11/site-packages/; exit"
-  
-# Download the Firefox driver and include it in the layer
-cd $project_root
-firefox_driver_url="https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz"
-curl -L $firefox_driver_url | tar -xz -C python
 
-# Download Firefox and include it in the layer
-firefox_url="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux"
-curl -L $firefox_url | tar -xz -C python
-
-# Create a zip file for the Lambda layer
-zip -r $output_path python
+zip -r $python_layer_output python
 
 # Clean up temporary directory
 rm -rf python
 
-echo "Lambda layer payload created at: $output_path"
+#===============================================================================
+# CHROME LAYER
+#===============================================================================
+
+CHROMEDRIVER_VERSION="2.32"
+HEADLESS_CHROMIUM_VERSION="1.0.0-29"
+CHROMELAYER_DIR="chromelayer"
+
+rm -rf chromelayer
+mkdir -p chromelayer/bin
+
+# Download and unzip chromedriver
+curl -L https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -o $CHROMELAYER_DIR/chromedriver.zip
+tar -xf $CHROMELAYER_DIR/chromedriver.zip -C $CHROMELAYER_DIR/bin/
+rm $CHROMELAYER_DIR/chromedriver.zip
+
+# Download and unzip headless chromium
+curl -L https://github.com/adieuadieu/serverless-chrome/releases/download/v$HEADLESS_CHROMIUM_VERSION/stable-headless-chromium-amazonlinux-2017-03.zip -o $CHROMELAYER_DIR/headless-chromium.zip
+tar -xf $CHROMELAYER_DIR/headless-chromium.zip -C $CHROMELAYER_DIR/bin/
+rm $CHROMELAYER_DIR/headless-chromium.zip
+
+# Download shared linux libs
+curl -SL https://github.com/21Buttons/pychromeless/blob/master/lib/libORBit-2.so.0?raw=true -o $CHROMELAYER_DIR/lib/libORBit-2.so.0
+curl -SL https://github.com/21Buttons/pychromeless/blob/master/lib/libgconf-2.so.4?raw=true -o $CHROMELAYER_DIR/lib/libgconf-2.so.4
+
+# Create a zip file for the Chrome Lambda layer
+zip -r $chrome_layer_output $CHROMELAYER_DIR
+
+# Clean up temporary directory
+rm -rf chromelayer
+
+echo "Lambda layers created at: $python_layer_output and $chrome_layer_output"
