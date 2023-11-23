c6a80185e990a056bcba4635b636f5c61f4b1bed
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-23 02:02:13 -0500

    No more zip file layer payloads

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,3 @@
 locals {
-  goose_nest_scraper_chrome_layer_payload_path = "${path.module}/../goose_nest_scraper_chrome_layer_payload.zip"
-  goose_nest_scraper_python_layer_payload_path = "${path.module}/../goose_nest_scraper_python_layer_payload.zip"
   goose_nest_scraper_lambda_function_payload_path = "${path.module}/../goose_nest_scraper_lambda_function_payload.zip"
 }
