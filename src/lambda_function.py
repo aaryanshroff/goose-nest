cc8f062d19b588a08309e303869e72d9177605ce
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-24 00:48:51 -0500

    It works!

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 
 def lambda_handler(event, context):
   options = webdriver.ChromeOptions()
-  options.binary_location = "/opt/bin/headless-chromium"
+  options.binary_location = "/opt/chrome-linux64/chrome"
   options.add_argument("--headless")
+  # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
 
-  service = webdriver.ChromeService(executable_path="/opt/bin/chromedriver")
+  service = webdriver.ChromeService(executable_path="/opt/chromedriver-linux64/chromedriver")
 
   browser = webdriver.Chrome(service=service, options=options)
 
