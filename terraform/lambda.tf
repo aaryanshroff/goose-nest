4f12c7aaa5627c6023269073b5bd6fa7384a9825
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-23 23:52:45 -0500

    Change Lambda TF to Image package type

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,23 +1,11 @@
-##############################################
-# Goose Nest Scraper Lambda Function
-##############################################
-data "archive_file" "goose_nest_scraper_lambda_function_payload" {
-  output_path = local.goose_nest_scraper_lambda_function_payload_path
-  type = "zip"
-
-  source_dir = "${path.module}/../src"
-}
-
 resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
-  filename      = local.goose_nest_scraper_lambda_function_payload_path
   function_name = "goose_nest_scraper_lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   timeout = 60
 
-  source_code_hash = data.archive_file.goose_nest_scraper_lambda_function_payload.output_base64sha256
+  image_uri = "${aws_ecr_repository.goose_nest_scraper_ecr_repository.repository_url}:latest"
+  package_type = "Image"
 
   runtime = "python3.11"
-
-  depends_on = [ data.archive_file.goose_nest_scraper_lambda_function_payload ]
 }
