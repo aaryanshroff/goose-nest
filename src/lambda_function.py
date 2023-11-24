5f31adf561f3bbd87d66eb8515b6556fa05e10cc
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-11-24 01:18:20 -0500

    Scrape

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,30 @@
-from selenium import webdriver
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from utils.webdriver_manager import setup_webdriver
 
 def lambda_handler(event, context):
   driver = setup_webdriver()
 
-  return "Hello, Selenium"
+  driver.get('https://facebook.com/marketplace/toronto/search?query=rentals')
+
+  wait = WebDriverWait(driver, 10)
+  collection_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Collection of Marketplace items"]')))
+
+  link_elements = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Collection of Marketplace items"] a')
+
+  entries = []
+  hrefs = []
+
+  for element in link_elements:
+      inner_text = element.text
+      href = element.get_attribute('href')
+
+      entries.append(inner_text)
+      hrefs.append(href)
+  
+  driver.quit()
+    
+  return json.dumps(entries)
