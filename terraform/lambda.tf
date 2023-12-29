8cb23b04f34643423482baa747194b25abe1752c
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-12-29 18:23:16 +0530

    Fix: Add APP env var to lambda func

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,10 +2,16 @@ resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
   function_name = "goose_nest_scraper_lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
-  timeout = 60
+  timeout       = 60
 
-  image_uri = "${aws_ecr_repository.goose_nest_scraper_ecr_repository.repository_url}:latest"
+  image_uri    = "${aws_ecr_repository.goose_nest_scraper_ecr_repository.repository_url}:latest"
   package_type = "Image"
 
   runtime = "python3.11"
+
+  environment {
+    variables = {
+      APP = "production"
+    }
+  }
 }
