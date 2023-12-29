6e88778d1d5deab584bd729b6908e2ed6ca3a5dc
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-12-29 18:38:10 +0530

    Fix: Add remote debugging port arg to chrome options

--- src/utils/webdriver_manager.py
+++ src/utils/webdriver_manager.py
@@ -4,21 +4,30 @@ from selenium.webdriver import Chrome
 
 from constants import APP
 
+
 def setup_webdriver():
-  chrome_options = webdriver.ChromeOptions()
-  if APP == "production":
-    chrome_options.binary_location = "/opt/chrome-linux64/chrome"
-  chrome_options.add_argument("--headless")
-  # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
-  chrome_options.add_argument("--no-sandbox")
-  chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options = webdriver.ChromeOptions()
+    if APP == "production":
+        chrome_options.binary_location = "/opt/chrome-linux64/chrome"
+    chrome_options.add_argument("--headless")
+    # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9230")
 
-  service = None
-  if APP == "production":
-    service = ChromeService(executable_path="/opt/chromedriver-linux64/chromedriver")
-  else:
-    service = ChromeService()
+    service = None
+    if APP == "production":
+        service = ChromeService(
+            executable_path="/opt/chromedriver-linux64/chromedriver")
+    else:
+        service = ChromeService()
 
-  driver = Chrome(service=service, options=chrome_options)
+    driver = Chrome(service=service, options=chrome_options)
 
-  return driver
+    return driver
+
+
+def shutdown_webdriver(driver: Chrome):
+    driver.stop_client()
+    driver.close()
+    driver.quit()
