f7e5f52671b35699be1220001310509abe49fd2e
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-01-02 19:15:12 +0530

    Increase timeout to 120s

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "goose_nest_scraper_lambda_function" {
   function_name = "goose_nest_scraper_lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
-  timeout       = 60
+  timeout       = 120
 
   image_uri    = "${aws_ecr_repository.goose_nest_scraper_ecr_repository.repository_url}:latest"
   package_type = "Image"
