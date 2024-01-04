aece4f831fefb5fce1367b942355fed0194cce28
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-01-04 15:59:05 +0530

    Mess with options to get scraper working on Lambda

--- src/utils/webdriver_manager.py
+++ src/utils/webdriver_manager.py
@@ -1,30 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver import FirefoxService
-from selenium.webdriver import Firefox
+from selenium.webdriver import ChromeService as Service
+from selenium.webdriver import Chrome as Browser
 
 from constants import APP
+from logger import LOGGER
+
+BROWSER_BINARY_LOCATION = "/opt/chrome/chrome"
+DRIVER_EXEC_PATH = "/opt/chromedriver/chromedriver"
 
 
 def setup_webdriver():
-    options = webdriver.FirefoxOptions()
+    options = webdriver.ChromeOptions()
     # TODO: Find way to DRY this location here and in Dockerfile
-    options.binary_location = "/opt/firefox/firefox"
-    options.add_argument("-headless")
+    options.binary_location = BROWSER_BINARY_LOCATION
+    options.add_argument("--headless=new")
     # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--remote-debugging-port=9230")
+    options.add_argument("--no-sandbox")
+    # TODO: How many of these do we actually need?
+    options.add_argument("--window-size=1280,1696")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--single-process")
+    options.add_argument("--no-zygote")
+    options.add_argument("--remote-debugging-port=9222")
 
     # TODO: Find way to DRY this location here and in Dockerfile
-    service = FirefoxService(
-        executable_path="/opt/geckodriver")
+    LOGGER.info("Initializing service...")
+    service = Service(
+        executable_path=DRIVER_EXEC_PATH)
+    LOGGER.info("Service initialization complete.")
 
-    driver = Firefox(service=service, options=options)
+    LOGGER.info("Initializing browser...")
+    browser = Browser(service=service, options=options)
+    LOGGER.info("Browser initialization complete.")
 
-    return driver
+    return browser
 
 
-def shutdown_webdriver(driver: Firefox):
+def shutdown_webdriver(driver: Browser):
     driver.stop_client()
     driver.close()
     driver.quit()
